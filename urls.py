2dc4f7b703b5ba3400c8ef592c0fa59658d1432e
Balqees2000 <113801932+Balqees2000@users.noreply.github.com>
2025-02-19 14:36:47 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,17 @@
-"""
-URL configuration for library_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# This file is the main entry for our application
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', include('library.urls')),  
-    path('admin/', admin.site.urls),
+    path('home', home),
+    path('books_view',books_view),
+    path('readers', readers_tab),
+    path('shopping', shopping),
+    path('save', save_customer),
+    path('readers/add', save_reader),
+    path('login', login),
+    path('books', books_tab),
+    path('books/add', save_book),
+    path('myFav',favourites),
+    path('books/add_favorite/<int:book_id>/', add_to_favorites, name='add_to_favorites'),
+    path('books/remove_favorite/<int:book_id>/', remove_from_favorites, name='remove_from_favorites'),
 ]
